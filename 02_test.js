0ef7920e0f6764a1798699965894d60b8d149c05
JerLalancette <lalancette.jerermie@gmail.com>
2018-02-07 11:17:56 -0500

    acces form et essait lire form

--- 02_test.js
+++ 02_test.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.get('/html/01_html.htm', function (req, res) {
+const fs = require('fs');
+
+app.get('/formulaire', (req, res) => {
  console.log(__dirname);
- res.sendFile( __dirname + "/" + "01_html.htm" );
+ res.sendFile( __dirname + "/public/html/" + "01_html.htm" );
 })
 
 app.get('/', (req, res) => {
@@ -11,7 +13,7 @@ app.get('/', (req, res) => {
  res.end('<h1>Accueil</h1>')
 })
 
-app.get('/traiter_get', function (req, res) {
+app.get('/traiter_get', (req, res) => {
  // Preparer l'output en format JSON
 
 console.log('la route /traiter_get')
@@ -24,12 +26,36 @@ console.log('la route /traiter_get')
  courriel:req.query.courriel
  };
 console.log(reponse);
- res.end(JSON.stringify(reponse));
+ res.end(JSON.stringify(reponse + ","));
+
+ fs.readFile('membres.json', 'utf-8', (err,data) => {
+    if (err) throw(err);
+    let liste = JSON.parse(data);
+    liste.push(reponse);
+    console.log('sauvé');
+
+    fs.readFile('membres.json', JSON.stringify(liste), (err, data) => {
+        if (err) throw(err);
+    }) ;
+
+}) ;
+
+});
+
+
+
+app.get('/membres', (req, res) => {
+
+    fs.readFile('membres.json', 'utf8', (err, data) => {
+        if (err) throw(err);
+        let membres = JSON.parse(data);
+    }) ;
+
 })
 
-var server = app.listen(8081, function () {
- var host = server.address().address
- var port = server.address().port
+const server = app.listen(8081, () => {
+ let host = server.address().address
+ let port = server.address().port
  
  console.log("Exemple l'application écoute sur http://%s:%s", host, port)
 
